bf15fe74ca1859bce78b90409e0fe378a1a2dd0b
chaitanya kumar <nunna.chaitanyakumar.imp@gmail.com>
2025-11-14 01:59:37 +0530

    changes

--- src/main.jsx
+++ src/main.jsx
@@ -32,20 +32,39 @@
 //   </React.StrictMode>
 // );
 
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import { BrowserRouter } from "react-router-dom";
+// import App from "./App";
+// import { ProductProvider } from "./context/ProductContext";
+// import "./index.css";
+
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//   <React.StrictMode>
+//     {/* 👇 Add basename to match your repo name exactly */}
+//     <BrowserRouter basename="/fertibaseadmin">
+//       <ProductProvider>
+//         <App />
+//       </ProductProvider>
+//     </BrowserRouter>
+//   </React.StrictMode>
+// );
+
+
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 import { ProductProvider } from "./context/ProductContext";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    {/* 👇 Add basename to match your repo name exactly */}
-    <BrowserRouter basename="/fertibaseadmin">
+    <HashRouter>
       <ProductProvider>
         <App />
       </ProductProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
+
